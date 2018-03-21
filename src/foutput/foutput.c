454ffcbb71ccd352b939ba3318af2164e31dae87
Michael Ries <michael.riess@mailfence.com>
2018-03-21 11:27:15 -0400

    wrote an foutput.c

---
+++ src/foutput/foutput.c
@@ -0,0 +1,15 @@
+#define OUTPUT_PROMPT Factors:
+
+/*used to turn macro into string*/
+#define _TEXTIFY(x) #x
+#define TEXTIFY(x) _TEXTIFY(x)
+
+
+char* foutput(double x1, double x2)
+{
+    char* output = calloc(1, sizeof(char));
+    char* temp = output;
+    asprintf(&output, "%s %lf, %lf\n", TEXTIFY(OUTPUT_PROMPT), x1, x2);
+    free(temp);
+    return output;
+}
